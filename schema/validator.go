b8e94b05f1c4e99e46305582ec4793a7302de370
yaiba <4yaiba@gmail.com>
2023-08-07 14:05:48 -0500

    feat: remove lexical mode

--- schema/validator.go
+++ schema/validator.go
@@ -1,33 +1,10 @@
 package schema
 
 import (
-	"errors"
 	"fmt"
 	"github.com/kwilteam/kwil-db/pkg/engine/dataset/actparser"
 	"github.com/kwilteam/kwil-db/pkg/engine/sqlparser"
 	"strings"
-)
-
-var (
-	ErrAttributeNotAllow  = errors.New("attribute not allowed")
-	ErrDuplicateTable     = errors.New("duplicate table")
-	ErrDuplicateExtension = errors.New("duplicate extension")
-	ErrDuplicateColumn    = errors.New("duplicate column")
-	ErrDuplicateAttribute = errors.New("duplicate attribute")
-	ErrDuplicateIndex     = errors.New("duplicate index")
-	ErrDuplicateAction    = errors.New("duplicate action")
-	ErrDuplicateParam     = errors.New("duplicate param")
-	ErrDupFKAction        = errors.New("duplicate foreign key action")
-	ErrDuplicateVariable  = errors.New("duplicate variable")
-	ErrTableNotFound      = errors.New("table not found")
-	ErrColumnNotFound     = errors.New("column not found")
-	ErrActionNotFound     = errors.New("action not found")
-	ErrVariableNotFound   = errors.New("variable not found")
-	ErrExtensionNotFound  = errors.New("extension not found")
-	ErrBlockVarNotFound   = errors.New("block variable not found")
-
-	// ErrMultiInit this error will be thrown in parsing stage.
-	ErrMultiInit = errors.New("more than one init")
 )
 
 var (
